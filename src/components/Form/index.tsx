102ff8e70723c98ff1387d7198ab7bab85b44d15
Pabllot07 <pabllot07@outlook.com>
2024-07-11 10:16:53 -0300

    style: container gap decreased

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -2,18 +2,53 @@ import TextField from "@mui/material/TextField";
 import MenuItem from "@mui/material/MenuItem";
 import { Button } from "@mui/material";
 
-import { Container } from "../Container";
 import { options } from "../../constants/select_options";
 import styles from "./styles.module.scss";
 
 export const Form = () => {
   return (
-    <Container>
-      <div className={styles.form}>
+    <div className={styles.wrapper}>
+      <TextField
+        id="outlined-read-only-input"
+        label="Nome completo"
+        defaultValue="João Linaldo Dias Fraga Santos"
+        InputProps={{
+          readOnly: true,
+        }}
+        className={styles.input}
+      />
+      <TextField
+        id="outlined-read-only-input"
+        label="CPF"
+        defaultValue="405.503.503-15"
+        InputProps={{
+          readOnly: true,
+        }}
+        className={styles.input}
+      />
+      <TextField
+        id="outlined-read-only-input"
+        label="Número do cartão"
+        defaultValue="405.503.503-15"
+        InputProps={{
+          readOnly: true,
+        }}
+        className={styles.input}
+      />
+      <TextField
+        id="outlined-read-only-input"
+        label="Número do cartão"
+        defaultValue="405.503.503-15"
+        InputProps={{
+          readOnly: true,
+        }}
+        className={styles.input}
+      />
+      <div className={styles.twoInARow}>
         <TextField
           id="outlined-read-only-input"
-          label="Nome completo"
-          defaultValue="João Linaldo Dias Fraga Santos"
+          label="Vencimento"
+          defaultValue="10/11"
           InputProps={{
             readOnly: true,
           }}
@@ -21,62 +56,24 @@ export const Form = () => {
         />
         <TextField
           id="outlined-read-only-input"
-          label="CPF"
-          defaultValue="405.503.503-15"
+          label="CVV"
+          defaultValue="405"
           InputProps={{
             readOnly: true,
           }}
           className={styles.input}
         />
-        <TextField
-          id="outlined-read-only-input"
-          label="Número do cartão"
-          defaultValue="405.503.503-15"
-          InputProps={{
-            readOnly: true,
-          }}
-          className={styles.input}
-        />
-        <TextField
-          id="outlined-read-only-input"
-          label="Número do cartão"
-          defaultValue="405.503.503-15"
-          InputProps={{
-            readOnly: true,
-          }}
-          className={styles.input}
-        />
-        <div className={styles.twoInARow}>
-          <TextField
-            id="outlined-read-only-input"
-            label="Vencimento"
-            defaultValue="10/11"
-            InputProps={{
-              readOnly: true,
-            }}
-            className={styles.input}
-          />
-          <TextField
-            id="outlined-read-only-input"
-            label="CVV"
-            defaultValue="405"
-            InputProps={{
-              readOnly: true,
-            }}
-            className={styles.input}
-          />
-        </div>
-        <TextField id="outlined-read-only-input" select defaultValue="1" className={styles.input}>
-          {options.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
-        <Button color="secondary" variant="contained">
-          Pagar
-        </Button>
       </div>
-    </Container>
+      <TextField id="outlined-read-only-input" select defaultValue="1" className={styles.input}>
+        {options.map((option) => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
+          </MenuItem>
+        ))}
+      </TextField>
+      <Button color="secondary" variant="contained">
+        <span className={styles.btn}>Pagar</span>
+      </Button>
+    </div>
   );
 };
