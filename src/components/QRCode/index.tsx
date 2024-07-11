e6e78666748bdc302e1049780b182ead72cccc2d
Pabllot07 <pabllot07@outlook.com>
2024-07-11 11:02:27 -0300

    style: notification when copying qrcode code

--- src/components/QRCode/index.tsx
+++ src/components/QRCode/index.tsx
@@ -1,18 +1,41 @@
-import { Button } from "@mui/material";
+import { useState } from "react";
+import { Alert, Button } from "@mui/material";
+import Snackbar from "@mui/material/Snackbar";
 
 import qrcode from "../../assets/qrcode.png";
 import copy from "../../assets/copy.png";
 import styles from "./styles.module.scss";
 
+//simulating data
+const qrcode_details = {
+  image: qrcode,
+  code: "posdkfIOFS78SDFPO9847584podkfg",
+};
+
 export const QRCode = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.qrcode_container}>
-        <img src={qrcode} />
+        <img src={qrcode_details.image} />
       </div>
-      <Button color="secondary" variant="contained" className={styles.btn}>
+      <Button color="secondary" variant="contained" className={styles.btn} onClick={handleClick}>
         <span className={styles.btn_text}>Clique para copiar QR CODE</span> <img src={copy} />
-      </Button>{" "}
+      </Button>
+      <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" variant="filled" sx={{ width: "100%" }}>
+          QR CODE copiado com sucesso.
+        </Alert>
+      </Snackbar>
     </div>
   );
 };
